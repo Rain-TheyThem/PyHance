0085b186d88fea7dfe51793a45015f09ab22b00e
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-06-09 20:06:06 +0000

    Change file structure

---
+++ main.py
@@ -0,0 +1,4 @@
+from pyhance.dict import dictionary
+
+di = dictionary({"a": 1, "b": 2})
+print(di.item_at(0))
--- pyhance/list.py
+++ pyhance/list.py
@@ -1,6 +1,8 @@
 class list_(list):
     def __init__(self, contents):
         self.li = list(contents)
+    def __repr__(self):
+        return str(self.li)
     def index_of(self, item):
         for index in range(len(self.li)):
             if self.li[index] == item:
