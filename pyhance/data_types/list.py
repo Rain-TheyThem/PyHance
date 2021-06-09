f4caee1ea3af41ec5c43f8b3c70e29bb81b59338
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-06-09 04:48:44 +0000

    Fixed index_of method of list_ class

--- pyhance/data_types/list.py
+++ pyhance/data_types/list.py
@@ -4,5 +4,5 @@ class list_(list):
     def index_of(self, item):
         for index in range(len(self.li)):
             if self.li[index] == item:
-                return item
+                return index
         raise ValueError("Item not in list")
