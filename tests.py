8c343b1016fb472e15c4b45539d7d684c046ad19
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-06-09 21:34:38 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+# destination: import village
+from pyhance.dict import dictionary as d
+
+# declare dictionary for testing
+di = d({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f", 6})
+
+# begin asserting!
+assert di.key_at(0) == "a"
+assert di.key_at(1) == "b"
+assert di.key_at(2) == "c"
