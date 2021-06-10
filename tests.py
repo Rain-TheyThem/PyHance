fc624ee0e702b280c02d7f1457cdce518f3682a9
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-06-09 21:36:15 -0400

    Update tests.py
    Checks for rest of keys

--- tests.py
+++ tests.py
@@ -8,3 +8,6 @@ di = d({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f", 6})
 assert di.key_at(0) == "a"
 assert di.key_at(1) == "b"
 assert di.key_at(2) == "c"
+assert di.key_at(3) == "d"
+assert di.key_at(4) == "e"
+assert di.key_at(5) == "f"
