e561ced4af65cab790c5554a7d33eb22d794607b
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-06-16 19:19:23 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@
 from pyhance.dict import dictionary as d
 
 # declare dictionary for testing
-di = d({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f", 6})
+di = d({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6})
 
 # begin asserting!
 
