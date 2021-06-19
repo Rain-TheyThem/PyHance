1b4020f14fc87cd297223fc305db986de7f4e5d9
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-06-18 20:44:36 -0400

    Add `copy` method to string class

--- pyhance/string.py
+++ pyhance/string.py
@@ -1,8 +1,14 @@
 class string(str):
     def __init__(self, string):
         self.s = str(string)
+    
+    def __repr__(self):
+        return self.s
+    
     def remove(self, string):
         self.s = self.s.replace(string, "")
         return self.s
-    def __repr__(self):
-        return self.s
+    
+    def copy(self):
+        newstr = ""
+        return newstr.join(self.s)
