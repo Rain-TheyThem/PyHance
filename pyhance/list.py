df07715fd83984ca0c8e6bdb13d8f42b5092d45a
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-06-18 22:28:33 -0400

    Update list.py
    Add method to remove all occurrences of an item in a list (the built-in `remove` method only removes the first occurrence)

--- pyhance/list.py
+++ pyhance/list.py
@@ -3,8 +3,5 @@ class list_(list):
         self.li = list(contents)
     def __repr__(self):
         return str(self.li)
-    def index_of(self, item):
-        for index in range(len(self.li)):
-            if self.li[index] == item:
-                return index
-        raise ValueError("Item not in list")
+    def remove_all(self, item):
+        self.li = [citem for citem in self.li if citem != item]
