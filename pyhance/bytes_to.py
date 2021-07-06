dfd59b8d10886a6be2c646fe82f3b4648562b032
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-07-05 23:51:22 -0400

    Update bytes_to.py
    Add functionality for string, str, and int

--- pyhance/bytes_to.py
+++ pyhance/bytes_to.py
@@ -1,11 +1,25 @@
 from pyhance import (
-  string,
-  list_,
-  dictionary
+    string,
+    list_,
+    dictionary
 )
 
-def bytes_to(datatype, encoding=None):
+def bytes_to(bytes_to_convert, datatype, method="little", byte_number=1):
     dtypes = (string, str, list, list_, dict, dictionary, tuple, int)
-    
+
     if datatype not in dtypes:
         raise ValueError(f"data type passed ({datatype}) not in available data types\n{dtypes}")
+    else:
+        if datatype == string:
+            return string(map(chr, bytes))
+        elif datatype == str:
+            return str(map(chr, bytes))
+        elif datatype == int:
+            if "method" in options:
+                if method in ("little", "big"):
+                    if type(byte_number) == int:
+                        return int.from_bytes(method(byte_number), method)
+                    else:
+                        raise TypeError(f"Invalid data type of byte_number ({byte_number}), type must be int")
+                else:
+                    raise ValueError(f"Invalid method ({method}), method must be 'big' or 'little'")
