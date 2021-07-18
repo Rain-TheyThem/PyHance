672eab7874c7f132d99444f16b68052f3761daf5
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-07-18 19:29:52 -0400

    Update dict.py
    Add docstrings

--- pyhance/dict.py
+++ pyhance/dict.py
@@ -1,8 +1,14 @@
 class dictionary(dict):
+    """
+    A custom dictionary class with all of the method from the built-in data type dict, while adding additional methods
+    """
     def __init__(self, key_value_pairs):
         self.d = dict(key_value_pairs)
 
     def key_at(self, index):
+        """
+        A method for indexing into the keys of the dictionary (finding the key at the specified index)
+        """
         if type(index) != int:
             raise TypeError(
                 f"index parameter was expecting int, {type(index)} when was passed"
@@ -15,6 +21,9 @@ class dictionary(dict):
         raise IndexError("dictionary index out of range")
 
     def value_at(self, index):
+        """
+        A method for indexing into the values of the dictionary (finding the value at the specified index)
+        """
         if type(index) != int:
             raise TypeError(
                 f"index parameter was expecting int, {type(index)} when was passed"
@@ -27,6 +36,9 @@ class dictionary(dict):
         raise IndexError("dictionary index out of range")
 
     def item_at(self, index):
+        """
+        A method for indexing into the key-value pairs of the dictionary (finding the key-value pair at the specified index)
+        """
         if type(index) != int:
             raise TypeError(
                 f"index parameter was expecting int, {type(index)} when was passed"
@@ -39,18 +51,27 @@ class dictionary(dict):
         raise IndexError("dictionary index out of range")
 
     def key_index(self, key):
+        """
+        A method for getting the index of a key
+        """
         for key_index, current_key in enumerate(self.d.keys()):
             if current_key == key:
                 return key_index
         raise ValueError(f"'{key}'' not in dictionary")
 
     def value_index(self, value):
+        """
+        A method for getting the index of a value
+        """
         for value_index, current_key in enumerate(self.d.values()):
             if current_key == value:
                 return value_index
         raise ValueError(f"'{value}' not in dictionary")
 
     def item_index(self, item):
+        """
+        A method for getting the index of a key
+        """
         for item_index, current_key in enumerate(self.d.items()):
             if {current_key[0]: self.d[current_key[0]]} == item:
                 return item_index
