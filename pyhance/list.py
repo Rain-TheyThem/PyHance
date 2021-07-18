972ca13f01e8dfa99e3c3699ddc7dc5fcbd4969f
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-07-18 19:31:35 -0400

    Update list.py
    Add docstring

--- pyhance/list.py
+++ pyhance/list.py
@@ -6,4 +6,7 @@ class list_(list):
         return str(self.li)
 
     def remove_all(self, item):
+        """
+        A method to remove all occurences of an item in the list
+        """
         self.li = [citem for citem in self.li if citem != item]
