ad4c6c6b01e457ea1c466158f8df18ca2fde351e
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-07-18 19:33:21 -0400

    Update range.py
    Add docstring

--- pyhance/range.py
+++ pyhance/range.py
@@ -1,4 +1,9 @@
 def range_(stop, start=0, step=1, inclusive=False):
+    """
+    A function to return a range of numbers,
+    starting at start (which defaults to 0),
+    ending at end, and increments each number by step (which defaults to 1), with an option to be inclusive.
+    """
     if inclusive:
         if start > stop:
             return range(start, stop - 1, step)
