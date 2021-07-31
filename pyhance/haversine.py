87756c667ebe928404caa92074a4511a30016f84
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-07-31 18:04:19 -0400

    Update haversine.py
    Update docstring to be more informative

--- pyhance/haversine.py
+++ pyhance/haversine.py
@@ -3,7 +3,13 @@ from math import sqrt
 
 def haversine(point1, point2, sigfigs=2):
     """
-    A standalone function to find the distance between two points, passed as tuples
+    A standalone function to find the distance between two points, passed as tuples.
+
+    Parameters
+    ----------
+    point1: the first tuple of coordinates
+    point2: the second tuple of coordinates
+    sigfigs: the number of decimal places to return. Pass None to return all sigfigs.
     """
     hdist = (point2[0] - point1[0])**2
     vdist = (point2[1] - point1[1])**2
